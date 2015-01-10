d82c0026dc54c76353d3a97b9d642e65c6e10315
Marius Horga <mhorga@gmail.com>
2015-01-10 12:39:55 +0200

    Initial Commit

---
+++ podTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podTesting
+//
+//  Created by Marius Horga on 1/10/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ podTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  podTesting
+//
+//  Created by Marius Horga on 1/10/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
